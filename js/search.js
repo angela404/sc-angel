c15857b67c67533606c2d6baa9411ed25801df0d
angela404 <angeliikaa404@gmail.com>
2024-09-09 13:22:57 +0700

    feat: add expand search

---
+++ js/search.js
@@ -0,0 +1,10 @@
+const btnSearch = (search, button) =>{
+    const searchBar = document.getElementById(search),
+          searchButton = document.getElementById(button)
+ 
+    searchButton.addEventListener('click', () =>{
+        // We add the show-search class, so that the search bar expands
+        searchBar.classList.toggle('show-search')
+    })
+ }
+ btnSearch('form-search', 'search_btn')
